f84e0b961fb2fa5bd575f280d01cdde2f7c917a7
Nicolas Ferre <nicolas.ferre@atmel.com>
2012-10-16 19:05:25 +0200

    ARM: at91: add LCD pdata for 9x5 LCD
    Temporary in board-dt
    
    Signed-off-by: Ludovic Desroches <ludovic.desroches@atmel.com>

--- arch/arm/mach-at91/board-dt.c
+++ arch/arm/mach-at91/board-dt.c
@@ -26,6 +26,64 @@
 
 #include "generic.h"
 
+#include <linux/fb.h>
+
+#include <video/atmel_lcdfb.h>
+#include <mach/atmel_hlcdc.h>
+
+/*
+ * LCD Controller
+ */
+static struct fb_videomode at91_tft_vga_modes[] = {
+	{
+		.name		= "LG",
+		.refresh	= 60,
+		.xres		= 800,		.yres		= 480,
+		.pixclock	= KHZ2PICOS(33260),
+
+		.left_margin	= 88,		.right_margin	= 168,
+		.upper_margin	= 8,		.lower_margin	= 37,
+		.hsync_len	= 128,		.vsync_len	= 2,
+
+		.sync		= 0,
+		.vmode		= FB_VMODE_NONINTERLACED,
+	},
+};
+
+static struct fb_monspecs at91fb_default_monspecs = {
+	.manufacturer	= "LG",
+	.monitor	= "LB043WQ1",
+
+	.modedb		= at91_tft_vga_modes,
+	.modedb_len	= ARRAY_SIZE(at91_tft_vga_modes),
+	.hfmin		= 15000,
+	.hfmax		= 17640,
+	.vfmin		= 57,
+	.vfmax		= 67,
+};
+
+/* Default output mode is TFT 24 bit */
+#define BPP_OUT_DEFAULT_LCDCFG5	(LCDC_LCDCFG5_MODE_OUTPUT_24BPP)
+
+/* Driver datas */
+static struct atmel_lcdfb_info __initdata ek_lcdc_data = {
+	.lcdcon_is_backlight		= true,
+	.alpha_enabled			= false,
+	.default_bpp			= 16,
+	/* Reserve enough memory for 32bpp */
+	.smem_len			= 800 * 480 * 4,
+	/* default_lcdcon2 is used for LCDCFG5 */
+	.default_lcdcon2		= BPP_OUT_DEFAULT_LCDCFG5,
+	.default_monspecs		= &at91fb_default_monspecs,
+	.guard_time			= 9,
+	.lcd_wiring_mode		= ATMEL_LCDC_WIRING_RGB,
+};
+
+struct of_dev_auxdata at91_auxdata_lookup[] __initdata = {
+	OF_DEV_AUXDATA("atmel,at91sam9x5-lcd", 0xf8038000, "atmel_hlcdfb_base", &ek_lcdc_data),
+	OF_DEV_AUXDATA("atmel,at91sam9x5-lcd", 0xf8038100, "atmel_hlcdfb_ovl", &ek_lcdc_data),
+	{ /* sentinel */ }
+};
 
 static const struct of_device_id irq_of_match[] __initconst = {
 
@@ -40,7 +98,7 @@ static void __init at91_dt_init_irq(void)
 
 static void __init at91_dt_device_init(void)
 {
-	of_platform_populate(NULL, of_default_bus_match_table, NULL, NULL);
+	of_platform_populate(NULL, of_default_bus_match_table, at91_auxdata_lookup, NULL);
 }
 
 static const char *at91_dt_board_compat[] __initdata = {
