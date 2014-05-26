9ea1e32cf970de411174fef2ff46c31d78993c61
Wenyou Yang <wenyou.yang@atmel.com>
2014-05-26 10:29:55 +0800

    ARM: at91: SAMA5D4: add pm support
    Signed-off-by: Wenyou Yang <wenyou.yang@atmel.com>

--- arch/arm/mach-at91/pm.c
+++ arch/arm/mach-at91/pm.c
@@ -241,7 +241,8 @@ static int at91_pm_enter(suspend_state_t state)
 				else if (cpu_is_at91sam9g45()
 					|| cpu_is_at91sam9x5()
 					|| cpu_is_at91sam9n12()
-					|| cpu_is_sama5d3())
+					|| cpu_is_sama5d3()
+					|| cpu_is_sama5d4())
 					memctrl = AT91_MEMCTRL_DDRSDR;
 #ifdef CONFIG_AT91_SLOW_CLOCK
 				/* copy slow_clock handler to SRAM, and call it */
