a308ea5fbcc4b3072794cd68600f6c76bda13676
Wenyou Yang <wenyou.yang@atmel.com>
2014-06-26 15:22:37 +0800

    i2c: at91: move pm clock operation to CCF
    Signed-off-by: Wenyou Yang <wenyou.yang@atmel.com>

--- drivers/i2c/busses/i2c-at91.c
+++ drivers/i2c/busses/i2c-at91.c
@@ -797,7 +797,7 @@ static int at91_twi_runtime_suspend(struct device *dev)
 {
 	struct at91_twi_dev *twi_dev = dev_get_drvdata(dev);
 
-	clk_disable(twi_dev->clk);
+	clk_disable_unprepare(twi_dev->clk);
 
 	return 0;
 }
@@ -806,7 +806,7 @@ static int at91_twi_runtime_resume(struct device *dev)
 {
 	struct at91_twi_dev *twi_dev = dev_get_drvdata(dev);
 
-	return clk_enable(twi_dev->clk);
+	return clk_prepare_enable(twi_dev->clk);
 }
 
 static const struct dev_pm_ops at91_twi_pm = {
