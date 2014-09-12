30ab07c044d2b8365bb57fe492e424eae439a231
Bo Shen <voice.shen@atmel.com>
2014-09-12 15:50:29 +0800

    ARM: at91: board-dt-sam9: add lcd info for at91sam9m10g45ek
    Signed-off-by: Bo Shen <voice.shen@atmel.com>

--- arch/arm/mach-at91/board-dt-sam9.c
+++ arch/arm/mach-at91/board-dt-sam9.c
@@ -31,6 +31,7 @@
 #include <linux/fb.h>
 #include <video/atmel_lcdfb.h>
 #include <mach/atmel_hlcdc.h>
+#include <mach/atmel_lcdc.h>
 
 /*
  * LCD Controller
@@ -83,6 +84,7 @@ static struct atmel_lcdfb_info __initdata ek_lcdc_data = {
 struct of_dev_auxdata at91_auxdata_lookup[] __initdata = {
 	OF_DEV_AUXDATA("atmel,at91sam9x5-lcd", 0xf8038000, "atmel_hlcdfb_base", &ek_lcdc_data),
 	OF_DEV_AUXDATA("atmel,at91sam9x5-lcd", 0xf8038100, "atmel_hlcdfb_ovl1", &ek_lcdc_data),
+	OF_DEV_AUXDATA("atmel,at91sam9g45-lcd", 0x00500000, "atmel_lcdfb", &ek_lcdc_data),
 	{ /* sentinel */ }
 };
 
@@ -120,6 +122,30 @@ static void __init at91_dt_device_init(void)
 		memcpy(at91fb_default_monspecs.monitor, monitor, 14);
 
 		printk("LCD parameters updated for at91sam9n12ek display module\n");
+	}
+
+	if (of_machine_is_compatible("atmel,at91sam9m10g45ek")) {
+		#define AT91SAM9G45_DEFAULT_LCDCON2		\
+				(ATMEL_LCDC_MEMOR_LITTLE \
+				 | ATMEL_LCDC_DISTYPE_TFT \
+				 | ATMEL_LCDC_CLKMOD_ALWAYSACTIVE)
+
+		/* set LCD configuration */
+		at91_tft_vga_modes[0].xres = 480;
+		at91_tft_vga_modes[0].yres = 272;
+		at91_tft_vga_modes[0].pixclock = KHZ2PICOS(9000),
+
+		at91_tft_vga_modes[0].left_margin = 1;
+		at91_tft_vga_modes[0].right_margin = 1;
+		at91_tft_vga_modes[0].upper_margin = 40;
+		at91_tft_vga_modes[0].lower_margin = 1;
+		at91_tft_vga_modes[0].hsync_len = 45;
+		at91_tft_vga_modes[0].vsync_len = 1;
+
+		ek_lcdc_data.default_lcdcon2 = AT91SAM9G45_DEFAULT_LCDCON2;
+		ek_lcdc_data.default_dmacon = ATMEL_LCDC_DMAEN;
+
+		printk("LCD parameters updated for at91sam9m10g45ek display module\n");
 	}
 
 	of_platform_populate(NULL, of_default_bus_match_table, at91_auxdata_lookup, NULL);
