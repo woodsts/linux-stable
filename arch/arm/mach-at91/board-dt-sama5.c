b9f1a2fb6b15f11c5f80aad85de45440b9f3f5ae
Nicolas Ferre <nicolas.ferre@atmel.com>
2015-01-22 16:54:50 +0100

    ARM: at91: fix PM initialization for newer SoCs
    Newer SoCs: at91sam9x5, at91sam9n12, sama5d3 and sama5d4 embed a DDR controller
    and have a different PMC status register layout than the at91sam9g45. Create
    another at91_sam9x5_pm_init() function to match this compatibility.
    
    Signed-off-by: Nicolas Ferre <nicolas.ferre@atmel.com>

--- arch/arm/mach-at91/board-dt-sama5.c
+++ arch/arm/mach-at91/board-dt-sama5.c
@@ -46,7 +46,7 @@ static void __init sama5_dt_device_init(void)
 						ksz8081_phy_fixup);
 	}
 
-	at91_sam9260_pm_init();
+	at91_sam9x5_pm_init();
 	of_platform_populate(NULL, of_default_bus_match_table, NULL, NULL);
 }
 
