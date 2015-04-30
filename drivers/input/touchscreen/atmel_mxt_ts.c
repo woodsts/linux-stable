40a3a811b679c4a7b4913f0e4d0930eff8bedd56
Ludovic Desroches <ludovic.desroches@atmel.com>
2015-04-30 14:27:50 +0200

    Input: atmel_mxt_ts: add a second try if initialization fails
    For an unknown reason, the maxtouch can not ack the i2c request when we
    want to communicate. Trying to establish the communication finally works
    after about 200 ms. So when probing the driver if the first time
    initilization fails then wait and try a second time.
    
    Signed-off-by: Ludovic Desroches <ludovic.desroches@atmel.com>

--- drivers/input/touchscreen/atmel_mxt_ts.c
+++ drivers/input/touchscreen/atmel_mxt_ts.c
@@ -3462,8 +3462,14 @@ static int mxt_probe(struct i2c_client *client, const struct i2c_device_id *id)
 	}
 
 	error = mxt_initialize(data);
-	if (error)
-		goto err_remove_mem_access;
+	if (error) {
+		/* Wait and try a second time */
+		msleep(MXT_RESET_TIME);
+		dev_warn(&client->dev, "Try a second time to init maxtouch\n");
+		error = mxt_initialize(data);
+		if (error)
+			goto err_remove_mem_access;
+	}
 
 	return 0;
 
