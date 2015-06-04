184e713c980a470f3621de9759ca3fb5eb6cab70
Nicolas Ferre <nicolas.ferre@atmel.com>
2015-06-04 17:37:41 +0200

    ARM: at91: sama5d2: add machine definition to board-dt-sama5
    Add sama5d2 specific initialization functions board compatibility string.
    
    Signed-off-by: Nicolas Ferre <nicolas.ferre@atmel.com>

--- arch/arm/mach-at91/generic.h
+++ arch/arm/mach-at91/generic.h
@@ -43,6 +43,7 @@ extern void __init at91rm9200_pm_init(void);
 extern void __init at91sam9260_pm_init(void);
 extern void __init at91sam9g45_pm_init(void);
 extern void __init at91sam9x5_pm_init(void);
+extern void __init sam5d2_pm_init(void);
 extern void __init sam5d3_pm_init(void);
 extern void __init sam5d4_pm_init(void);
 #else
@@ -50,6 +51,7 @@ void __init at91rm9200_pm_init(void) { }
 void __init at91sam9260_pm_init(void) { }
 void __init at91sam9g45_pm_init(void) { }
 void __init at91sam9x5_pm_init(void) { }
+void __init sam5d2_pm_init(void) {}
 void __init sam5d3_pm_init(void) {}
 void __init sam5d4_pm_init(void) {}
 #endif
